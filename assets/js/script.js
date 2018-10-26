146d4bf37ae9f1a3155791314c13ab8bc6b00ca2
Ulric Todman <rick.sinclair.nyc@gmail.com>
2018-10-25 21:13:50 -0400

    started tv layout and working video

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,6 +19,8 @@ var television = {
 
     userAnswer: "",
 
+    timers: function() {},
+
     questionBank: [
       //This is the object array that holds all of the questions
       //and answers. My goal was to create a structure that
@@ -41,17 +43,37 @@ var television = {
         D:
           "Grandmaster Flash, Melle Mel, The Kidd Creole, Keef Cowboy, Mr. Ness/Scorpio, and Rahiem",
         CorrectAnswer:
-          "Grandmaster Flash, Melle Mel, The Kidd Creole, Keef Cowboy, Mr. Ness/Scorpio, and Rahiem"
+          "Grandmaster Flash, Melle Mel, The Kidd Creole, Keef Cowboy, Mr. Ness/Scorpio, and Rahiem",
+
+        actionSet: function() {
+          var imgUrl = "";
+          var soundClipUrl = "";
+          var video = $("<video />", {
+            id: "video-element",
+            src: "http://video-js.zencoder.com/oceans-clip.mp4",
+            type: "video/mp4",
+            controls: false,
+            loop: true,
+            muted: false,
+            style: "width:100%; height:100%;"
+          });
+          video.appendTo($("#tv-pic"));
+        }
       },
+
       {
         question:
-          "Who is the 'Lifestylez ov da Poor and Dangerous' rapper who famously freestyled with  Jay-Z 'Stretch and Bobbito' in 1995?",
+          "Who is the 'Lifestylez ov da Poor and Dangerous' rapper who famously freestyled with  Jay-Z on 'Stretch and Bobbito' in 1995?",
 
         A: "Big Sherm",
         B: "'Lil Pistol Starter",
         C: "Big L",
         D: "Stack Bundles",
-        CorrectAnswer: "Big L"
+        CorrectAnswer: "Big L",
+        actionset: function() {
+          var imgUrl = "";
+          var soundClipUrl = "";
+        }
       },
       {
         question:
@@ -61,88 +83,156 @@ var television = {
         B: "Wu-Tang Clan",
         C: "The Notorious B.I.G.",
         D: "Eric B. and Rakim.",
-        CorrectAnswer: "Wu-Tang Clan"
+        CorrectAnswer: "Wu-Tang Clan",
+        actionset: function() {
+          var imgUrl = "";
+          var soundClipUrl = "";
+        }
       },
       {
-        question: "",
+        question:
+          "What popular rap artist has a god-mother on the FBI Most Wanted List?",
 
-        A: "",
-        B: "",
-        C: "",
-        D: "",
-        CorrectAnswer: ""
+        A: "2PAC",
+        B: "Lil' Wayne",
+        C: "Young Jeezy",
+        D: "Jay-z",
+        CorrectAnswer: "2PAC",
+        actionset: function() {
+          var imgUrl = "";
+          var soundClipUrl = "";
+        }
       },
       {
-        question: "",
+        question:
+          "Who is the  self-dubbed  'Susan Lucci of rap' who has been nominated over thirteen times, but has never won a grammy?",
 
-        A: "",
-        B: "",
-        C: "",
-        D: "",
-        CorrectAnswer: ""
-      },
-      {
-        question: "",
-
-        A: "",
-        B: "",
-        C: "",
-        D: "",
-        CorrectAnswer: ""
-      },
-      {
-        question: "",
-
-        A: "",
-        B: "",
-        C: "",
-        D: "",
-        CorrectAnswer: ""
-      },
-      {
-        question: "",
-
-        A: "",
-        B: "",
-        C: "",
-        D: "",
-        CorrectAnswer: ""
-      },
-      {
-        question: "",
-
-        A: "",
-        B: "",
-        C: "",
-        D: "",
-        CorrectAnswer: ""
-      },
-      {
-        question: "",
-
-        A: "",
-        B: "",
-        C: "",
-        D: "",
-        CorrectAnswer: ""
-      },
-      {
-        question: "",
-
-        A: "",
-        B: "",
-        C: "",
-        D: "",
-        CorrectAnswer: ""
+        A: "Dr. Dre",
+        B: "Nas",
+        C: "Busta Rhymes",
+        D: "DMX",
+        CorrectAnswer: "Nas",
+        actionset: function() {
+          var imgUrl = "";
+          var soundClipUrl = "";
+        }
       }
     ],
 
-    //this function will activate the beginning of the game.
-    startGame: function() {},
+    //this contains the game mechanics.
+    gameMechanics: {
+      x: 0,
+      drawQuestion: function(indexNumber) {
+        TriviaGame.gameMechanics.fadeAndClear();
+        var CurrentQuestion = TriviaGame.questionBank[indexNumber].question;
+        var answerA = TriviaGame.questionBank[indexNumber].A;
+        var answerB = TriviaGame.questionBank[indexNumber].B;
+        var answerC = TriviaGame.questionBank[indexNumber].C;
+        var answerD = TriviaGame.questionBank[indexNumber].D;
+        var CorrectAnswer =
+          television.TriviaGame.questionBank[indexNumber].CorrectAnswer;
+
+        var form = $("<form></form>");
+        form.attr("id", "question-card");
+        var question = $("<h2></h2>").text(CurrentQuestion);
+        form.append(question);
+        var rule1;
+        var rule1;
+        var rule1;
+        var rule1;
+        var choiceA = $("<input />", {
+          type: radio,
+          id: "choiceA",
+          value: answerA
+        });
+        question.append(choicA);
+
+        var choiceALabel = $("<input />", {
+          for: "choiceA"
+        });
+        choiceALabel.append(ChoiceA);
+        choiceA.append(choiceALabel);
+        var choiceB = $("<input />", {
+          type: radio,
+          id: "choiceB",
+          value: answerB
+        });
+        choiceALabel.append(choiceALabel);
+
+        var choiceBLabel = $("<input />", {
+          id: "choiceB",
+          value: answerB
+        });
+        choiceBLabel.append(choiceB);
+        var choiceC = $("<input />", {
+          id: "choiceB",
+          value: answerB
+        });
+        var choiceC = $("<input />", {
+          id: "choiceC",
+          value: answerC
+        });
+        choiceA.append(form);
+
+        var choiceALabel = $("<input />", {
+          for: "choiceA"
+        });
+        choiceALabel.append(ChoiceA);
+
+        var choiceB = $("<input />", {
+          id: "choiceB",
+          value: answerB
+        });
+        choiceB.append(choiceALabel);
+
+        var choiceBLabel = $("<input />", {
+          id: "choiceB",
+          value: answerB
+        });
+        choiceBLabel.append(choiceB);
+        var choiceC = $("<input />", {
+          id: "choiceA",
+          value: answerA
+        });
+
+        var choiceD = $("").text(CurrentQuestion);
+        var quizForm = $(question).attr("id", "question");
+      },
+
+      fadeGameSpace: function() {
+        $("#video-element").hide();
+        $("#game-space").hide();
+        $("#game-space").fadeIn(5000, "linear");
+        $("#video-element").fadeIn(5000, "linear");
+        $("#game-space").fadeOut(8000, "linear");
+        $("#video-element").fadeOut(9000, "linear");
+      },
+
+      clearGameSpace: function() {}
+    },
 
     //this function will control our timer
     timer: function() {}
   }
 };
 
-console.log(television.TriviaGame.questionBank);
+television.TriviaGame.gameMechanics.fadeGameSpace();
+// $("document").ready(function() {
+//   console.log(television.TriviaGame.questionBank);
+//   //   $("#game-space").fadeOut(1600, "linear");
+//   television.TriviaGame.gameMechanics.fadeGameSpace();
+//   console.log(television.TriviaGame.gameMechanics.fadeGameSpace);
+
+//   $("#video-src").attr("src", "../video/funny video.webm");
+// });
+// //FOUND THIS VIDEO CLEARING SOLUTION https://stackoverflow.com/questions/3258587/how-to-properly-unload-destroy-a-video-element
+
+// var videoElement = $("#video-element");
+// videoElement.pause();
+// videoElement.removeAttribute("src"); // empty source
+
+// videoElement.load();
+// videoElement.src("../video/funny video.webm");
+// television.TriviaGame.questionBank[0].actionSet();
+
+//ALSO FOUND THIS ON W3SCHOOLS https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_av_met_load
